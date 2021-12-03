ef33ba2405c0a597ae9e7e246ff14726a448b44b
azenaishvili <azenaishvili@AZENAISHVILI1>
2021-12-03 16:45:48 +0400

    update

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,7 @@
-const API_PREFIX = 'https://localhost:44314'
-// const API_PREFIX = 'https://weblive.com.ge'
+// const API_PREFIX = 'https://localhost:44314'
+const API_PREFIX = 'https://weblive.com.ge'
 
 export default {
     // eslint-disable-next-line no-undef
     API_PREFIX,
   }
-  
