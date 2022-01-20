ac24dca00d0f98a66a4345d65d69d62a648ea0f4
azenaishvili <azenaishvili@AZENAISHVILI1>
2022-01-20 10:45:20 +0400

    up

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
 // const API_PREFIX = 'https://localhost:5002'
 // const API_PREFIX = 'https://localhost:4287'
 const API_PREFIX = 'https://weblive.com.ge'
+// const API_PREFIX = 'http://ranko-prod.eu-central-1.elasticbeanstalk.com'
+// const API_PREFIX = 'https://main.d3ip4su9ylspny.amplifyapp.com'
 
 export default {
     // eslint-disable-next-line no-undef
