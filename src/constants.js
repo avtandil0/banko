1596e4ab4c82fc52157f9c3baf007734e00c4847
avtandil0 <avtandilzenaishvili@gmail.com>
2022-02-03 11:52:49 +0400

    update

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,8 @@
 // const API_PREFIX = 'https://localhost:4287'
 // const API_PREFIX = 'https://localhost:44314'
 // const API_PREFIX = 'https://weblive.com.ge'
-const API_PREFIX = 'http://avtandil-002-site3.ftempurl.com'
+// const API_PREFIX = 'http://avtandil-002-site3.ftempurl.com'
+const API_PREFIX = 'http://avtandil-002-site3.itempurl.com'
 // const API_PREFIX = 'http://ranko-prod.eu-central-1.elasticbeanstalk.com'
 // const API_PREFIX = 'https://main.d3ip4su9ylspny.amplifyapp.com'
 
